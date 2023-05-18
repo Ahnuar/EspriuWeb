191b0f84e15266d07f0804a50e958e08548dc11d
ManelMayoral <manelmayoralcano7@gmail.com>
2023-05-18 16:31:12 +0200

    retoques

--- resources/views/eventos/block.blade.php
+++ resources/views/eventos/block.blade.php
@@ -7,37 +7,37 @@
         @if($evento->url_google_maps!=null && $evento->url_google_maps!='')
            <div class="w-100"> {!! $evento->url_google_maps !!}</div>
         @endif
+        <br>
         @if(count($hijos)>0)
-        <div class="d-flex justify-content-end">
+        <div class="row">
             <form method="POST" action="{{ route('evento.signup') }}" name="inscribir{{$evento->id}}" id="inscribir{{$evento->id}}">
                 @csrf
                 <input type="text" hidden value="{{$evento->id}}" id="idEvento" name="idEvento">
-                <select name="selectHijo" id="selectHijo" form="inscribir{{$evento->id}}">
+                <select name="selectHijo" id="selectHijo" form="inscribir{{$evento->id}}" class="form-control">
                     @foreach($hijos as $hijo)
                         <option value="{{$hijo->id}}">{{$hijo->nombre}} {{$hijo->apellidos}}</option>
                     @endforeach
                 </select>
                 <br>
-                <button type="submit" class="btn btn-primary text-end">INSCRIURE</a>
+                <button type="submit" class="btn btn-primary  w-100">INSCRIURE</a>
             </form>
-            
         </div>
         @endif
-
     </div>
     @if(isset($success))
-    @if($evento->id ==$eventoInscrito)
-    @if($success)
-    <div class="alert alert-success text-center">
-        Infant {{$fillInscrit}} inscrit!
-    </div>
+        @if($evento->id ==$eventoInscrito)
+            @if($success)
+                <div class="alert alert-success text-center">
+                    Infant {{$fillInscrit}} inscrit!
+                </div>
+            @endif
+            @if(!$success)
+                <div class="alert alert-danger text-center">
+                    Ja has inscrit a {{$fillInscrit}} en aquest event!
+                </div>
+            @endif
+        @endif
     @endif
-    @if(!$success)
-    <div class="alert alert-danger text-center">
-        Ja has inscrit a {{$fillInscrit}} en aquest event!
-    </div>
-    @endif
-    @endif
-@endif
 </div>
+
 
--- resources/views/monitorfunc/gestionmonitor.blade.php
+++ resources/views/monitorfunc/gestionmonitor.blade.php
@@ -5,28 +5,32 @@
             <div class="card">
                 <div class="card-header">Funcions de Monitor:</div>
                 <div class="card-body">
-                    <div class="btn-group mr-2" role="group" aria-label="First group">
-                        <form action="{{route('lista')}}">
-                            <button class="btn btn-primary adminfunc" >Passar llista</button>
-                        </form>
-                        <form action="{{route('exportar')}}">
-                            <button class="btn btn-primary adminfunc" >Exportar Mensual</button>
-                            <select name="mes" id="mes">
-                                <option value="1">Gener</option>
-                                <option value="2">Febrer</option>
-                                <option value="3">Març</option>
-                                <option value="4">Abril</option>
-                                <option value="5">Maig</option>
-                                <option value="6">Juny</option>
-                                <option value="7">Juliol</option>
-                                <option value="8">Agost</option>
-                                <option value="9">Septembre</option>
-                                <option value="10">Octubre</option>
-                                <option value="11">Novembre</option>
-                                <option value="12">Decembre</option> 
-                            </select>
-                        </form>
-                        
+                    <div class="row">
+                            <div class="col-12 col-md-6">
+                                <form action="{{route('lista')}}">
+                                    <button class="btn btn-primary text-center w-100" >Passar llista</button>
+                                </form>
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <form action="{{route('exportar')}}">
+                                    <button class="btn btn-primary text-center w-100" >Exportar Mensual</button>
+                                    <select name="mes" id="mes" class="form-control text-center w-100" >
+                                        <option value="1">Gener</option>
+                                        <option value="2">Febrer</option>
+                                        <option value="3">Març</option>
+                                        <option value="4">Abril</option>
+                                        <option value="5">Maig</option>
+                                        <option value="6">Juny</option>
+                                        <option value="7">Juliol</option>
+                                        <option value="8">Agost</option>
+                                        <option value="9">Septembre</option>
+                                        <option value="10">Octubre</option>
+                                        <option value="11">Novembre</option>
+                                        <option value="12">Decembre</option> 
+                                    </select>
+                                </form>
+                            
+                        </div>                        
                     </div>
                 </div>
 
